089da903e85465ac9555c9f8eae91338aeb7cae4
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-09 08:25:09 +0530

    Added neccessary libraries for the project like react-query, react-router, react-hook-form, react-hot-toast, react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Navigate, Route } from "react-router";
 
 export default function App() {
-  return <div className="bg-amber-50">Certus Admin</div>;
+  return (
+    <div className="text-center text-xl font-semibold text-yellow-500">
+      Certus Diagnostics Admin
+    </div>
+  );
 }
